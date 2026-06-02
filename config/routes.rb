99ef4f6a0a4380440351a6368daa7b1a83bb3748
Tom LOPEZ <tomlpz.pro2@gmail.com>
2026-06-02 16:46:22 +0200

    create registration role Dreamer & Maker

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+devise_for :users, controllers: {
+  registrations: 'users/registrations'
+}
+
   root to: "pages#home"
   get "up" => "rails/health#show", as: :rails_health_check
 
