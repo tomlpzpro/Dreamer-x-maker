19c6b751c1f0dba31d96f4d90f01f3c0f805a93e
Tom LOPEZ <tomlpz.pro2@gmail.com>
2026-06-03 16:57:02 +0200

    feat: OpenAI image generation working

--- app/controllers/ai_chats_controller.rb
+++ app/controllers/ai_chats_controller.rb
@@ -1,27 +1,31 @@
 class AiChatsController < ApplicationController
   def show
-    # Find the chat we want to display
     @llm_chat = LlmChat.find(params[:id])
-    # All the messages of this chat, oldest first
     @llm_messages = @llm_chat.llm_messages.order(:created_at)
-    # Empty message used by the form at the bottom of the page
     @llm_message = LlmMessage.new
   end
 
   def create_message
-    # Find the chat we are writing in
     @llm_chat = LlmChat.find(params[:id])
-    # Build a new message for this chat
     @llm_message = @llm_chat.llm_messages.new(llm_message_params)
-    # The user is the one writing this message
     @llm_message.role = "user"
     if @llm_message.save
       redirect_to ai_chat_path(@llm_chat)
     else
-      # If saving fails, show the chat page again
       @llm_messages = @llm_chat.llm_messages.order(:created_at)
       render :show, status: :unprocessable_entity
     end
+  end
+
+  def generer_visuel
+    @llm_chat = LlmChat.find(params[:id])
+    image = RubyLLM.paint(@llm_chat.project.description)
+    @llm_chat.project.image.attach(
+      io: StringIO.new(Base64.decode64(image.data)),
+      filename: "generated_#{@llm_chat.project.id}.png",
+      content_type: "image/png"
+    )
+    redirect_to ai_chat_path(@llm_chat), notice: "Visuel généré !"
   end
 
   private
