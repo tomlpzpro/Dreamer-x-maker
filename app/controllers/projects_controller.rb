137707f48a8b7543ae4fa271307b592d9329196c
Tom LOPEZ <tomlpz.pro2@gmail.com>
2026-06-03 11:30:59 +0200

    create project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,44 @@
 class ProjectsController < ApplicationController
-  def index
-  end
-
   def show
+    @project = Project.find(params[:id])
   end
 
   def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.dreamer = current_user
+    if @project.save
+      redirect_to @project, notice: "Project was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to @project, notice: "Project was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_url, notice: "Project was successfully destroyed."
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description)
   end
 end
