518587231a43d177f22d58ae4e8f95bc42ff896f
Tom LOPEZ <tomlpz.pro2@gmail.com>
2026-06-04 11:59:40 +0200

    feat: show generer_visuel button only after assistant message and without image

--- app/controllers/ai_chats_controller.rb
+++ app/controllers/ai_chats_controller.rb
@@ -10,6 +10,8 @@ class AiChatsController < ApplicationController
     @llm_message = @llm_chat.llm_messages.new(llm_message_params)
     @llm_message.role = "user"
     if @llm_message.save
+      response = RubyLLM.chat.ask(@llm_message.content)
+      @llm_chat.llm_messages.create(role: "assistant", content: response.content)
       redirect_to ai_chat_path(@llm_chat)
     else
       @llm_messages = @llm_chat.llm_messages.order(:created_at)
