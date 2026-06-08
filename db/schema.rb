0e3a38ca4c2137d34a43fbf9b6c839bebf7419f6
claire frabolot <claire.frabolot@gmail.com>
2026-06-08 12:10:03 +0200

    schema diff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_05_101025) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_05_104521) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -78,7 +78,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_05_101025) do
     t.string "content"
     t.datetime "created_at", null: false
     t.bigint "match_chat_id", null: false
-    t.boolean "read"
+    t.boolean "read", default: false, null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["match_chat_id"], name: "index_match_messages_on_match_chat_id"
